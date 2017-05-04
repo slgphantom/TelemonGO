c166771dca17ffd2468ac23d6b26b0033fcc4d49
LORD-SS <slgphantom@gmail.com>
2017-05-04 12:20:50 +0800

    Update check_iv_cp.py

--- check_iv_cp.py
+++ check_iv_cp.py
@@ -4,27 +4,7 @@
 import sys
 import time
 import telepot
-import requests
 from functions import check_iv_cp, show_map, gps
-import json
-
-#import for pgoapi
-from pgoapi import PGoApi
-from pgoapi.hash_server import HashServer
-from pgoapi import utilities as util
-import pprint
-import random
-api = PGoApi()
-
-#logging
-import logging
-logger = logging.getLogger('TelemonGO')
-logger.setLevel(logging.DEBUG)
-logger2 = logging.StreamHandler()
-logger2.setLevel(logging.ERROR)
-logger.addHandler(logger2)
-formatter = logging.Formatter('[%(asctime)s] - [%(levelname)s] - [line %(lineno)d] - [%(message)s]')
-logger2.setFormatter(formatter)
 
 ##+++++++++++++++++++++++Edit below items+++++++++++++++++++++++++++#
 #hash_key = "<hashkey>"
@@ -37,13 +17,6 @@ logger2.setFormatter(formatter)
 #bot = telepot.Bot("<bot_token>")
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
 
-ac_sum = 0
-place = None
-
-with open('pkmn_hk.json') as json_name:
-    pkm_hk = json.load(json_name)
-with open('pkm_mv.json') as json_mv:
-    pkm_mv = json.load(json_mv)
 
 def handle(msg):
     global ac_sum
