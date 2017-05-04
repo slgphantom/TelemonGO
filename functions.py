13db1d6d010302bd31513d7b3d2a0f4c2f27e782
LORD-SS <slgphantom@gmail.com>
2017-05-04 15:59:27 +0800

    import config.ini

--- functions.py
+++ functions.py
@@ -6,6 +6,7 @@ import time
 import telepot
 import requests
 from urllib import urlopen
+import ConfigParser
 import json
 
 #import for pgoapi
@@ -26,16 +27,16 @@ logger.addHandler(logger2)
 formatter = logging.Formatter('[%(asctime)s] - [%(levelname)s] - [line %(lineno)d] - [%(message)s]')
 logger2.setFormatter(formatter)
 
-##+++++++++++++++++++++++Edit below items+++++++++++++++++++++++++++#
-#hash_key = "<hashkey>"
-#ac_list = ["<username>"]
-#pw_list = ["<password>"]
-#home_lat = float(<lat>)
-#home_lng = float(<lng>)
-#allowed_list = [<telegram_id>]
-#own_id = <telegram_id>
-#bot = telepot.Bot("<bot_token>")
-##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+#config
+hash_key = config.get('DEFAULT', 'hash_key')
+ac_list = eval(config.get('account', 'user'))
+pw_list = eval(config.get('account', 'pass'))
+home_lat = config.getfloat('Location', 'lat')
+home_lng = config.getfloat('Location', 'lng')
+allowed_list = eval(config.get('Telegram', 'allow_list'))
+own_id = config.getint('Telegram', 'admin_id')
+bot_token = config.get('Telegram', 'bot_token')
+bot = telepot.Bot(bot_token)
 
 ac_sum = 0
 place = None
