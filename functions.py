90773562d1c45a41dc25b4a8240da1dce918fe69
LORD-SS <slgphantom@gmail.com>
2017-05-04 12:20:55 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,49 @@
-# encoding=utf8 
+# encoding=utf8  
+
+#import for telegram
+import sys
+import time
+import telepot
+import requests
+from urllib import urlopen
+import json
+
+#import for pgoapi
+from pgoapi import PGoApi
+from pgoapi.hash_server import HashServer
+from pgoapi import utilities as util
+import pprint
+import random
+api = PGoApi()
+
+#logging
+import logging
+logger = logging.getLogger('TelemonGO')
+logger.setLevel(logging.DEBUG)
+logger2 = logging.StreamHandler()
+logger2.setLevel(logging.ERROR)
+logger.addHandler(logger2)
+formatter = logging.Formatter('[%(asctime)s] - [%(levelname)s] - [line %(lineno)d] - [%(message)s]')
+logger2.setFormatter(formatter)
+
+##+++++++++++++++++++++++Edit below items+++++++++++++++++++++++++++#
+#hash_key = "<hashkey>"
+#ac_list = ["<username>"]
+#pw_list = ["<password>"]
+#home_lat = float(<lat>)
+#home_lng = float(<lng>)
+#allowed_list = [<telegram_id>]
+#own_id = <telegram_id>
+#bot = telepot.Bot("<bot_token>")
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+
+ac_sum = 0
+place = None
+
+with open('pkmn_hk.json') as json_name:
+    pkm_hk = json.load(json_name)
+with open('pkm_mv.json') as json_mv:
+    pkm_mv = json.load(json_mv)
 
 def calc_level(pokemon_data):
     cpm = pokemon_data["cp_multiplier"]
