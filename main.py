327aaf8c60944b92a7bc80420fd32c00926d93ff
LORD-SS <slgphantom@gmail.com>
2017-05-16 00:10:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 import time
 import telepot
 import ConfigParser
-from functions import check_iv_cp, show_map, gps
+from functions import check_iv_cp, show_map, gps, wecatch
 
 #config
 config = ConfigParser.ConfigParser()
