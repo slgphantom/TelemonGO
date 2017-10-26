f43ec8c8e948b34f59ae757dda1992aa6661976a
LORD-SS <slgphantom@gmail.com>
2017-10-26 17:53:57 +0800

    Refactoring main.py

--- main.py
+++ main.py
@@ -1,37 +1,43 @@
-# encoding=utf8  
+# encoding=utf8
 
-#import for telegram
+#-----import-----#
+import ConfigParser
 import sys
 import time
-import telepot
-import ConfigParser
-from functions import check_iv_cp, show_map, gps, wecatch
 
-#config
+import telepot
+
+from functions import *
+#----------#
+
+#-----config-----#
 config = ConfigParser.ConfigParser()
 config.readfp(open('config.ini'))
 
 allowed_list = eval(config.get('Telegram', 'allow_list'))
 bot_token = config.get('Telegram', 'bot_token')
 bot = telepot.Bot(bot_token)
+#----------#
 
-
+#-----Telegram Handler-----#
 def handle(msg):
-    global ac_sum
-
     #Access Control
     if msg['chat']['id'] in allowed_list:
         check_iv_cp(msg)
-        gps(msg)
-        show_map(msg)
+        get_gps(msg)
+        get_map(msg)
         wecatch(msg)
+        poketrack(msg)
     else:
-        gps(msg)
-        show_map(msg)
+        get_gps(msg)
+        get_map(msg)
         wecatch(msg)
+        poketrack(msg)
+
 
 bot.message_loop({'chat': handle})
 print('Listening ...')
+#----------#
 
 while 1:
     time.sleep(10)
